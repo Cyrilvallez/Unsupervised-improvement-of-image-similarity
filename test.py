7e0c91ce82342adb0ba6c2741f0c0d240ada9e50
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-04 16:57:28 +0200

    first test

---
+++ test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  4 16:49:11 2022
+
+@author: cyrilvallez
+"""
+
+import numpy as np
+import faiss
+
+d = 64                           # dimension
+nb = 100000                      # database size
+nq = 10000                       # nb of queries
+np.random.seed(1234)             # make reproducible
+xb = np.random.random((nb, d)).astype('float32')
+xb[:, 0] += np.arange(nb) / 1000.
+xq = np.random.random((nq, d)).astype('float32')
+xq[:, 0] += np.arange(nq) / 1000.
+
+
+index = faiss.IndexFlatL2(d)   # build the index
+print(index.is_trained)
+index.add(xb)                  # add vectors to the index
+print(index.ntotal)
