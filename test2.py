ce632b873c7018f8d1ab93e917ac1431977419bc
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-09 16:43:45 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -9,32 +9,40 @@ Created on Fri May  6 14:44:42 2022
 from Extract_features import neural_features as nf
 from Extract_features import datasets
 
-model_name = 'SimCLR v2 ResNet50 2x'
+from tqdm import tqdm
+import psutil
 
-model = nf.MODEL_LOADER[model_name](device='cuda')
-transforms = nf.MODEL_TRANSFORMS[model_name]
+# model_name = 'SimCLR v2 ResNet50 2x'
 
-dataset = datasets.FlickrDataset(transforms=transforms)
+# model = nf.MODEL_LOADER[model_name](device='cuda')
+# transforms = nf.MODEL_TRANSFORMS[model_name]
 
-nf.extract_features(model, dataset, batch_size=1024)
+# dataset = datasets.FlickrDataset(transforms=transforms)
+
+# nf.extract_features(model, dataset, batch_size=1024)
 
 #%%
 
-# from Extract_features import neural_features as nf
-# from Extract_features import datasets
-# from torch.utils.data import DataLoader
+from Extract_features import neural_features as nf
+from Extract_features import datasets
+from torch.utils.data import DataLoader
 
-# if __name__ == '__main__':
+import psutil
 
-    # model_name = 'SimCLR v2 ResNet50 2x'
-    # transforms = nf.MODEL_TRANSFORMS[model_name]
+model_name = 'SimCLR v2 ResNet50 2x'
+transforms = nf.MODEL_TRANSFORMS[model_name]
 
-    # dataset = datasets.ImageDataset('Datasets/Flickr500K/0', transforms=transforms)
+dataset = datasets.ImageDataset('Datasets/Flickr500K/0', transforms=transforms)
 
-    # dataloader = DataLoader(dataset, batch_size=1028, shuffle=False, drop_last=True,
-                        # num_workers=2)
+dataloader = DataLoader(dataset, batch_size=1024, shuffle=False, drop_last=True)
 
-    # for images, names in dataloader:
-        # pass
+print(f'Before : ram {psutil.virtual_memory().used/1e9:.2f} Gb')
+
+i = 0
+for images, names in dataloader:
+    i += 1
+    print(f'Iter {i} ram : {psutil.virtual_memory().used/1e9:.2f} Gb')
+        
+    
 
 
