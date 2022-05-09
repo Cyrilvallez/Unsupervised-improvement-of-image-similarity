2aadaaebecb1513d7a3b2fa88fee5de0a514b85b
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-09 17:31:37 +0200

    Update neural_features.py

--- Extract_features/neural_features.py
+++ Extract_features/neural_features.py
@@ -288,18 +288,18 @@ def extract_features(model, dataset, batch_size=256, workers=4):
         with torch.no_grad():
             feats = model(images).cpu().numpy()
             
-        names = np.expand_dims(np.array(names), axis=1)
+        # names = np.expand_dims(np.array(names), axis=1)
 
         # First column is the identifier of the image
-        feats = np.concatenate((names,feats), axis=1)
+        # feats = np.concatenate((names,feats), axis=1)
         
-        try:
-            features = np.vstack((features, feats))
+        # try:
+            # features = np.vstack((features, feats))
         # It is not defined in 1st iteration
-        except NameError:
-            features = feats
+        # except NameError:
+            # features = feats
     
-    return features
+    # return features
            
 
 def extract_and_save_features(model, dataset, filename, batch_size=256, workers=4):
