3fcc058d18b403df13db65137b01a0f9273ee098
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-11 15:41:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,25 +14,33 @@ method = 'SimCLR v2 ResNet50 2x'
 dataset = 'Kaggle_templates'
 dataset_retrieval = 'Kaggle_memes'
 
+t0 = time.time()
+
 features, mapping = utils.combine_features(method, dataset)
 features_search, mapping_search = utils.load_features(method, dataset_retrieval)
+
+t1 = time.time()
+
+print(f'Time for loading data : {t1 - t0:.2f} s', flush=True)
 
 res = faiss.StandardGpuResources()  # use a single GPU
 
 index = faiss.IndexFlatL2(features.shape[1])
 index = faiss.index_cpu_to_gpu(res, 0, index)
 
-t0 = time.time()
+t2 = time.time()
+
+print(f'Time for creating index: {t2 - t1:.2f} s', flush=True)
 
 index.add(features)
 
-t1 = time.time()
+t3 = time.time()
 
-print(f'Time for adding to index : {t1 - t0:.2f} s', flush=True)
+print(f'Time for adding to index : {t3 - t2:.2f} s', flush=True)
 
 k = 1
 D, I = index.search(features_search, k)
 
-t2 = time.time() 
+t4 = time.time() 
 
-print(f'Time for search : {t2 - t1:.2f} s', flush=True)
+print(f'Time for search : {t4 - t3:.2f} s', flush=True)
