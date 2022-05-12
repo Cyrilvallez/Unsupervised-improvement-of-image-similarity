5660df31e36eec7556bc7ef0bd26e5b2ef850e59
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-12 18:27:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from helpers import utils
 import time
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import numpy as np
+import pickle
 
 method = 'SimCLR v2 ResNet50 2x'
 dataset = 'Kaggle_templates'
@@ -18,73 +20,124 @@ dataset_retrieval = 'Kaggle_memes'
 
 features_db, mapping_db = utils.combine_features(method, dataset)
 features_query, mapping_query = utils.load_features(method, dataset_retrieval)
+features_db_normalized = utils.normalize(features_db)
+features_query_normalized = utils.normalize(features_query)
 
 d = features_db.shape[1]
-
-indices = []
-names = []
-
 res = faiss.StandardGpuResources()  # use a single GPU
+nlist = int(10*np.sqrt(features_db.shape[0]))
 
-index = faiss.IndexFlat(d)
-index.metric_type = faiss.METRIC_JensenShannon
-indices.append(faiss.index_cpu_to_gpu(res, 0, index))
-names.append('JS Flat') 
-indices.append(faiss.index_cpu_to_gpu(res, 0, faiss.IndexFlatIP(d)))
-names.append('cosine Flat')
-indices.append(faiss.index_cpu_to_gpu(res, 0, faiss.IndexFlatL2(d)))
-names.append('L2 Flat')
-index = faiss.IndexFlat(d)
-index.metric_type = faiss.METRIC_L1
-indices.append(faiss.index_cpu_to_gpu(res, 0, index))
-names.append('L1 Flat')
+indices = [
+    utils.create_flat_index(res, d, 'JS'),
+    utils.create_flat_index(res, d, 'cosine'),
+    utils.create_flat_index(res, d, 'L2'),
+    utils.create_flat_index(res, d, 'L1'),
+    utils.create_IVFFlat_index(res, d, nlist, 'JS'),
+    utils.create_IVFFlat_index(res, d, nlist, 'cosine'),
+    utils.create_IVFFlat_index(res, d, nlist, 'L2'),
+    utils.create_IVFFlat_index(res, d, nlist, 'L1'),
+    ]
+names = [
+    'JS Flat',
+    'cosine Flat',
+    'L2 Flat',
+    'L1 Flat',
+    'JS IVFFlat',
+    'cosine IVFFlat',
+    'L2 IVFFlat',
+    'L1 IVFFlat',
+    ]
 
 recalls = []
 times = []
 
 k = 1
 
-for i in tqdm(range(len(indices))):
+for i in tqdm(len(indices)):
     
     index = indices[i]
     
-    t0 = time.time()
-    
-    if 'cosine' in names[i]:
-        index.add(utils.normalize(features_db))
-        D, I = index.search(utils.normalize(features_query), k)
-    
-    else:
-        index.add(features_db)
-        D, I = index.search(features_query, k)
+    if 'Flat' in names[i]:
         
-    times.append(time.time() - t0)
+        t0 = time.time()
+    
+        if 'cosine' in names[i]:
+            index.add(features_db_normalized)
+            D, I = index.search(features_query_normalized, k)
+    
+        else:
+            index.add(features_db)
+            D, I = index.search(features_query, k)
+        
+        times.append(time.time() - t0)
 
-    recall, _ = utils.recall(I, mapping_db, mapping_query)
-    recalls.append(recall)
+        recall, _ = utils.recall(I, mapping_db, mapping_query)
+        recalls.append(recall)
+        
+    elif 'IVFFlat' in names[i]:
+        
+        if 'cosine' in names[i]:
+            index.train(features_db_normalized)
+            index.add(features_db_normalized)           
+            
+        else:
+            index.train(features_db)
+            index.add(features_db)
+            
+        recall_list = []
+        time_list = []
+        
+        for nprobe in [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]:
+            
+            index.setNumProbes(nprobe)
+            
+            t0 = time.time()
+            
+            if 'cosine' in names[i]:
+                D, I = index.search(features_query_normalized, k)
+            else:
+                D, I = index.search(features_query, k)
+            
+            recall, _ = utils.recall(I, mapping_db, mapping_query)
+            recall_list.append(recall)
+            time_list.append(time.time() - t0)
+            
+        recalls.append(recall_list)
+        times.append(time_list)
     
     # Free memory while keeping list length consant
     indices[i] = 0
     del index
-
+    
+with open('recall.pickle', "wb") as fp:   #Pickling
+    pickle.dump(recalls, fp)
+    
+with open('time.pickle', "wb") as fp:   #Pickling
+    pickle.dump(times, fp)
 
 plt.figure()
 for i in range(len(names)):
-    if 'JS' in names[i]:
+    if names[i] == 'JS Flat':
         plt.scatter(recalls[i], times[i], color='r', marker='*', s=100)
-    elif 'cosine' in names[i]:
+    if names[i] == 'cosine Flat':
         plt.scatter(recalls[i], times[i], color='b', marker='o', s=100)
-    elif 'L2' in names[i]:
+    if names[i] == 'L2 Flat':
         plt.scatter(recalls[i], times[i], color='g', marker='x', s=100)
-    elif 'L1' in names[i]:
+    if names[i] == 'L1 Flat':
         plt.scatter(recalls[i], times[i], color='m', marker='s', s=100)
+    if names[i] == 'JS IVFFlat':
+        plt.plot(recalls[i], times[i], color='r')
+    if names[i] == 'cosine IVFFlat':
+        plt.plot(recalls[i], times[i], color='b')
+    if names[i] == 'L2 IVFFlat':
+        plt.plot(recalls[i], times[i], color='g')
+    if names[i] == 'L1 IVFFlat':
+        plt.plot(recalls[i], times[i], color='m')
 
 plt.xlabel('Recall@1')
 plt.ylabel('Search time [s]')
 plt.legend(names)
         
-plt.savefig('test.pdf', bbox_inches='tight')
+plt.savefig('benchmark.pdf', bbox_inches='tight')
 plt.show()
         
-
-
