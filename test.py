7a3482b26c5b3d202dbd412fb2003388a0892c3d
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-17 17:33:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -22,16 +22,16 @@ d = features_db.shape[1]
 
 res = faiss.StandardGpuResources()  
 
-coarse = faiss.IndexFlat(d)
-coarse.metric_type = faiss.METRIC_JensenShannon
-index1 = faiss.index_cpu_to_gpu(res, 0, coarse)
+# coarse = faiss.IndexFlat(d)
+# coarse.metric_type = faiss.METRIC_JensenShannon
+# coarse = faiss.index_cpu_to_gpu(res, 0, coarse)
 
-# nlist = int(10*np.sqrt(features_db.shape[0]))
-# index = faiss.IndexIVFFlat(coarse, d, nlist)
+nlist = int(10*np.sqrt(features_db.shape[0]))
+# index1 = faiss.IndexIVFFlat(coarse, d, nlist)
 
-# factory_string = f'IVF{nlist}'
-# index1 = faiss.index_factory(d, factory_string)
-# index1 = faiss.index_cpu_to_gpu(res, 0, index1)
+factory_string = f'IVF{nlist}, Flat'
+index1 = faiss.index_factory(d, factory_string)
+index1 = faiss.index_cpu_to_gpu(res, 0, index1)
 
 t0 = time.time()
 
@@ -48,37 +48,37 @@ t2 = time.time()
 
 print(f'Searching time : {t2 - t1:.2f} s', flush=True)
 
-recall, _ = utils.recall(I1, mapping_db, mapping_query)
+# recall, _ = utils.recall(I1, mapping_db, mapping_query)
 
-print(f'Recall : {recall:.2f}')
+# print(f'Recall : {recall:.2f}')
 
-# del index1
+del index1
 
-# index2 = faiss.index_factory(d, factory_string)
+index2 = faiss.index_factory(d, factory_string)
 
-# index_ivf = faiss.extract_index_ivf(index2)
-# clustering_index = faiss.index_cpu_to_all_gpus(faiss.IndexFlatL2(index_ivf.d))
-# index_ivf.clustering_index = clustering_index
+index_ivf = faiss.extract_index_ivf(index2)
+clustering_index = faiss.index_cpu_to_all_gpus(faiss.IndexFlatL2(index_ivf.d))
+index_ivf.clustering_index = clustering_index
 
-# index2 = faiss.index_cpu_to_all_gpus(index2)
+index2 = faiss.index_cpu_to_all_gpus(index2)
 
 
-# t3 = time.time()
-# index2.train(features_db)
-# index2.add(features_db)
-# t4 = time.time()
+t3 = time.time()
+index2.train(features_db)
+index2.add(features_db)
+t4 = time.time()
 
-# print(f'Training time : {t4 - t3:.2f} s', flush=True)
+print(f'Training time : {t4 - t3:.2f} s', flush=True)
 
-# D2,I2 = index2.search(features_query, 1)
+D2,I2 = index2.search(features_query, 1)
 
-# t5 = time.time()
+t5 = time.time()
 
-# print(f'Searching time : {t5 - t4:.2f} s', flush=True)
+print(f'Searching time : {t5 - t4:.2f} s', flush=True)
 
-# assert ((D1==D2).all())
-# assert ((I1==I2).all())
-# print('Everything is same')
+assert ((D1==D2).all())
+assert ((I1==I2).all())
+print('Everything is same')
 
 #%%
 
