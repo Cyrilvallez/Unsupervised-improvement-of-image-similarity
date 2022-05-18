d87202788dc81fa6f154c8031dc80fdd2f782460
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-18 16:14:29 +0200

    plot

--- helpers/plot.py
+++ helpers/plot.py
@@ -5,31 +5,35 @@ Created on Wed May 18 15:54:40 2022
 
 @author: cyrilvallez
 """
+import matplotlib.pyplot as plt
 
-"""
-plt.figure()
-for i in range(len(names)):
-    if names[i] == 'JS Flat':
-        plt.scatter(recalls[i], times[i], color='r', marker='*', s=100)
-    if names[i] == 'cosine Flat':
-        plt.scatter(recalls[i], times[i], color='b', marker='o', s=100)
-    if names[i] == 'L2 Flat':
-        plt.scatter(recalls[i], times[i], color='g', marker='x', s=100)
-    if names[i] == 'L1 Flat':
-        plt.scatter(recalls[i], times[i], color='m', marker='s', s=100)
-    if names[i] == 'JS IVFFlat':
-        plt.plot(recalls[i], times[i], color='r')
-    if names[i] == 'cosine IVFFlat':
-        plt.plot(recalls[i], times[i], color='b')
-    if names[i] == 'L2 IVFFlat':
-        plt.plot(recalls[i], times[i], color='g')
-    if names[i] == 'L1 IVFFlat':
-        plt.plot(recalls[i], times[i], color='m')
+from helpers import plot_config
 
-plt.xlabel('Recall@1')
-plt.ylabel('Search time [s]')
-plt.legend(names)
+
+def time_recall_plot(results, save=False, filename=None):
+    
+    plt.figure()
+    for i in range(len(names)):
+        if names[i] == 'JS Flat':
+            plt.scatter(recalls[i], times[i], color='r', marker='*', s=100)
+        if names[i] == 'cosine Flat':
+            plt.scatter(recalls[i], times[i], color='b', marker='o', s=100)
+        if names[i] == 'L2 Flat':
+            plt.scatter(recalls[i], times[i], color='g', marker='x', s=100)
+        if names[i] == 'L1 Flat':
+            plt.scatter(recalls[i], times[i], color='m', marker='s', s=100)
+        if names[i] == 'JS IVFFlat':
+            plt.plot(recalls[i], times[i], color='r')
+        if names[i] == 'cosine IVFFlat':
+            plt.plot(recalls[i], times[i], color='b')
+        if names[i] == 'L2 IVFFlat':
+            plt.plot(recalls[i], times[i], color='g')
+        if names[i] == 'L1 IVFFlat':
+            plt.plot(recalls[i], times[i], color='m')
+
+    plt.xlabel('Recall@1')
+    plt.ylabel('Search time [s]')
+    plt.legend(names)
         
-plt.savefig('benchmark.pdf', bbox_inches='tight')
-plt.show()
-"""
+    plt.savefig('benchmark.pdf', bbox_inches='tight')
+    plt.show()
