ca0d3ec7dccffa1bc27fa17ab7bf6c79b9113660
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-20 11:17:07 +0200

    repro_bug

---
+++ repro_bug.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 11:13:08 2022
+
+@author: cyrilvallez
+"""
+
+import numpy as np
+import faiss
+from tqdm import tqdm
+
+features_db = np.random.rand(500000, 4096)
+features_query = np.random.rand(500000, 4096)
+
+d = features_db.shape[1]
+nlist = int(10*np.sqrt(features_db.shape[0]))
+factory_string = f'IVF{nlist},Flat'
+
+
+# Works fine
+indices = []
+indices.append(faiss.index_factory(d, factory_string, faiss.METRIC_L2))
+indices.append(faiss.index_factory(d, factory_string, faiss.METRIC_INNER_PRODUCT))
+
+for i in tqdm(range(len(indices))):
+    
+    index = indices[i]
+    index = faiss.index_cpu_to_all_gpus(index)
+    
+    index.train(features_db)
+    index.add(features_db)
+    
+    D, I = index.search(features_query, 1)
