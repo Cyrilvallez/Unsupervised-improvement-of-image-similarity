36db5fa67c933b0d40d0ecf448dda9441a7784eb
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-23 16:04:02 +0200

    Update extract_features.py

--- extract_features.py
+++ extract_features.py
@@ -7,6 +7,7 @@ Created on Fri May  6 14:44:42 2022
 """
 
 import extractor
+from tqdm import tqdm
 
 # method_name = 'SimCLR v2 ResNet50 2x'
 method_name = 'Dhash'
@@ -18,13 +19,13 @@ datasets = extractor.VALID_DATASET_NAMES
 
 # Perceptual
 if 'hash' in method_name:
-    for dataset in datasets:
+    for dataset in tqdm(datasets):
         dataset = extractor.create_dataset(dataset, None)
         extractor.extract_and_save_perceptual(method_name, dataset, hash_size=hash_size)
        
 # Neural
 else:
-    for dataset in datasets:
+    for dataset in tqdm(datasets):
         transforms = extractor.MODEL_TRANSFORMS[method_name]
         dataset = extractor.create_dataset(dataset, transforms)
         extractor.extract_and_save_neural(method_name, dataset, batch_size=batch_size, workers=3)
