054ca21c0d600383409869eb58e3139c27812dd1
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-25 16:00:30 +0200

    Update extractor.py

--- extractor/extractor.py
+++ extractor/extractor.py
@@ -198,13 +198,11 @@ def extract_and_save_perceptual(algorithm, dataset, hash_size=8,
 
     """
     
-    dataset_and_algo_name = dataset.name + '-' + '_'.join(algorithm.split(' ')) \
-        + f'_{hash_size**2} bits'
-    
     features, indices_to_names = extract_perceptual(algorithm, dataset, hash_size=hash_size,
                                                     batch_size=batch_size, workers=workers)
     
-    dataset_and_algo_name = dataset.name + '-' + '_'.join(algorithm.split(' '))
+    dataset_and_algo_name = dataset.name + '-' + '_'.join(algorithm.split(' ')) \
+        + f'_{hash_size**2} bits'
     
     np.save('Features/' + dataset_and_algo_name + '_features.npy', features)
     np.save('Features/' + dataset_and_algo_name + '_map_to_names.npy', indices_to_names)
