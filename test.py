782f3481900838f07f457b0d7b78fc4e7ee2ef45
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-31 15:30:38 +0200

    up

--- test.py
+++ test.py
@@ -8,16 +8,31 @@ Created on Wed May  4 16:49:11 2022
 
 import numpy as np
 import faiss
+import time
 
+from helpers import utils
 
-features = np.random.choice(255, size=(500000, 64)).astype('uint8')
-query = np.random.choice(255, size=(2000, 64)).astype('uint8')
+algorithm = 'Dhash 64 bits'
+main_dataset = 'BSDS500_original'
+distractor_dataset = 'Flickr500K'
+query_dataset = 'BSDS500_attacks'
 
-index = faiss.IndexBinaryFlat(features.shape[1]*8)
+features_db, mapping_db = utils.combine_features(algorithm, main_dataset, distractor_dataset)
+features_query, mapping_query = utils.load_features(algorithm, query_dataset)
+
+index = faiss.IndexBinaryFlat(features_db.shape[1]*8)
 # index = faiss.index_cpu_to_all_gpus(index)
-res = faiss.StandardGpuResources()
-index = faiss.index_cpu_to_gpu(res, 0, index)
+# res = faiss.StandardGpuResources()
+# index = faiss.index1_cpu_to_gpu(res, 0, index)
 
-index.add(features)
-D, I = index.search(query, 10)
+t0 = time.time()
 
+index.add(features_db)
+D, I = index.search(features_query, 10)
+
+dt = time.time() - t0
+
+recall, _ = utils.recall(I, mapping_db, mapping_query)
+
+print(f'{dt:.2f} s')
+print(f'Recall : {recall:.2f}')
