e52a3fa840883a54c430ccd4e568c3151776f0e9
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-05-31 13:35:10 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,34 +7,17 @@ Created on Wed May  4 16:49:11 2022
 """
 
 import numpy as np
-import shutil
-from PIL import Image
-from helpers import utils
-import torch
 import faiss
 
-algorithm = 'SimCLR v2 ResNet50 2x'
-dataset = 'Kaggle_memes'
 
-features, _ = utils.load_features(algorithm, dataset)
-# features = torch.tensor(features).to('cuda')
+features = np.random.choice(2, size=(500000, 64))
+query = np.random.choice(2, size=(2000, 64))
 
-# res = torch.cdist(features, features).cpu().numpy()
-# np.save('distances.npy', res)
-
-#%%
-
-index = faiss.IndexFlatIP(features.shape[1])
+index = faiss.IndexBinaryFlat(features.shape[1])
 index = faiss.index_cpu_to_all_gpus(index)
 
 index.add(features)
-D, I = index.search(features, 10)
+D, I = index.search(query, 10)
 
-res = np.zeros((features.shape[0], features.shape[0]), dtype='float32')
-
-for i in range(len(I)):
-    res[i, I[i,:]] = D[i, :]
-    
-np.save('distances_neighbors.npy', res)
 
 
