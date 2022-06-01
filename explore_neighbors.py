c80b1a9b50330b65c8a41dbfcfcaf2c9dd35b0d5
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-06-01 17:14:18 +0200

    Update explore_neighbors.py

--- explore_neighbors.py
+++ explore_neighbors.py
@@ -13,13 +13,13 @@ import os
 
 # algorithm = 'SimCLR v2 ResNet50 2x'
 algorithm = 'Dhash 64 bits'
-main_dataset = 'BSDS500_original'
-# main_dataset = 'Kaggle_templates'
+# main_dataset = 'BSDS500_original'
+main_dataset = 'Kaggle_templates'
 distractor_dataset = 'Flickr500K'
-query_dataset = 'BSDS500_attacks'
-# query_dataset = 'Kaggle_memes'
+# query_dataset = 'BSDS500_attacks'
+query_dataset = 'Kaggle_memes'
 
-save_folder = 'Results/Neighbors_BSDS500_flat_Dhash/'
+save_folder = 'Results/Neighbors_memes_flat_Dhash/'
 
 factory_str = 'BFlat'
 
