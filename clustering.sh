b9fbc7a39dada0417411f7b7cdbdadb982339749
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-06-01 16:50:31 +0200

    Update clustering.sh

--- clustering.sh
+++ clustering.sh
@@ -4,8 +4,8 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=16000
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32000
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:a100:1
 #SBATCH --chdir=/cluster/raid/home/cyril.vallez/Project2
