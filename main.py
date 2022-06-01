a0b359161b5bb518d4bd31cc2c8ded696caabde0
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-06-01 16:52:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ filename = save_folder + 'results.json'
 ex.compare_k_Flat(ks, algorithm, main_dataset, query_dataset,
                         distractor_dataset, filename)
 # ex.compare_nprobe_IVF(nlist, nprobes, algorithm, main_dataset, query_dataset,
-                        # distractor_dataset, filename, k=10, binary=False)
+                        # distractor_dataset, filename, k=10)
