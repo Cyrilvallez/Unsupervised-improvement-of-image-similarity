8829a30b0a983c53f30bb488ac65b306f3d78888
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-06-24 08:46:37 +0200

    up

--- finetuning/loss.py
+++ finetuning/loss.py
@@ -9,6 +9,8 @@ Created on Wed Jun 22 15:20:16 2022
 import torch
 import torch.nn as nn
 import torch.distributed as dist
+import math
+from torch import Tensor
 
 
 class Gather(torch.autograd.Function):
@@ -74,6 +76,17 @@ def gather(tensor):
     
     
 class NT_Xent(nn.Module):
+    """
+    Class implementing the NT-Xent loss function.
+
+    Parameters
+    ----------
+    temperature : float
+        The temperature.
+    epsilon : float, optional
+        A small number for numerical stability. The default is 1e-7.
+
+    """
     
     def __init__(self, temperature, epsilon=1e-7):
         
@@ -175,7 +188,7 @@ class GatherLayer(torch.autograd.Function):
     
 class NT_Xent2(nn.Module):
     def __init__(self, batch_size, temperature, world_size):
-        super(NT_Xent, self).__init__()
+        super(NT_Xent2, self).__init__()
         self.batch_size = batch_size
         self.temperature = temperature
         self.world_size = world_size
@@ -217,5 +230,74 @@ class NT_Xent2(nn.Module):
         logits = torch.cat((positive_samples, negative_samples), dim=1)
         loss = self.criterion(logits, labels)
         loss /= N
+        return loss
+    
+    
+    
+class SyncFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, tensor):
+        ctx.batch_size = tensor.shape[0]
+
+        gathered_tensor = [torch.zeros_like(tensor) for _ in range(torch.distributed.get_world_size())]
+
+        torch.distributed.all_gather(gathered_tensor, tensor)
+        gathered_tensor = torch.cat(gathered_tensor, 0)
+
+        return gathered_tensor
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_input = grad_output.clone()
+        torch.distributed.all_reduce(grad_input, op=torch.distributed.ReduceOp.SUM, async_op=False)
+
+        idx_from = torch.distributed.get_rank() * ctx.batch_size
+        idx_to = (torch.distributed.get_rank() + 1) * ctx.batch_size
+        return grad_input[idx_from:idx_to]
+    
+    
+class NT_Xent3(nn.Module):
+    
+    def __init__(self):
+        super(NT_Xent3, self).__init__()
+        
+        
+    def forward(self, out_1, out_2, temperature, eps=1e-6):
+        """
+        assume out_1 and out_2 are normalized
+        out_1: [batch_size, dim]
+        out_2: [batch_size, dim]
+        """
+        # gather representations in case of distributed training
+        # out_1_dist: [batch_size * world_size, dim]
+        # out_2_dist: [batch_size * world_size, dim]
+        if torch.distributed.is_available() and torch.distributed.is_initialized():
+            out_1_dist = SyncFunction.apply(out_1)
+            out_2_dist = SyncFunction.apply(out_2)
+        else:
+            out_1_dist = out_1
+            out_2_dist = out_2
+
+        # out: [2 * batch_size, dim]
+        # out_dist: [2 * batch_size * world_size, dim]
+        out = torch.cat([out_1, out_2], dim=0)
+        out_dist = torch.cat([out_1_dist, out_2_dist], dim=0)
+
+        # cov and sim: [2 * batch_size, 2 * batch_size * world_size]
+        # neg: [2 * batch_size]
+        cov = torch.mm(out, out_dist.t().contiguous())
+        sim = torch.exp(cov / temperature)
+        neg = sim.sum(dim=-1)
+
+        # from each row, subtract e^(1/temp) to remove similarity measure for x1.x1
+        row_sub = Tensor(neg.shape).fill_(math.e ** (1 / temperature)).to(neg.device)
+        neg = torch.clamp(neg - row_sub, min=eps)  # clamp for numerical stability
+
+        # Positive similarity, pos becomes [2 * batch_size]
+        pos = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+        pos = torch.cat([pos, pos], dim=0)
+
+        loss = -torch.log(pos / (neg + eps)).mean()
+
         return loss
     
