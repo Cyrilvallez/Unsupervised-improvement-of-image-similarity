fb7791af835528e38706d83121e0c4bc3180476b
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-06-29 14:08:20 +0200

    Update finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=20000
+#SBATCH --mem=50000
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:a100:8
 #SBATCH --chdir=/cluster/raid/home/cyril.vallez/Project2
