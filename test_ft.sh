8f5e3b14bc96b2cbe1efefb87e3d76d3640ae8d2
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-06-30 16:22:02 +0200

    Update test_ft.sh

--- test_ft.sh
+++ test_ft.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=3000
 #SBATCH --partition=nodes
 #SBATCH --gres=gpu:a100:1
