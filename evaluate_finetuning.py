98a528a3cf23e47c0967dc0ff4c41984c6b94f31
Cyril Vallez <71554963+Cyrilvallez@users.noreply.github.com>
2022-07-05 14:02:47 +0200

    Update evaluate_finetuning.py

--- evaluate_finetuning.py
+++ evaluate_finetuning.py
@@ -16,7 +16,7 @@ dataset = extractor.all_memes_dataset(transforms)
 path = 'first_test_models/2022-06-30_20:03:28/epoch_10.pth'
 model = SimCLR.load_encoder(path)
 
-features, mapping = extractor.extract_neural(model, dataset, batch_size=512)
+features, mapping = extractor.extract_neural(model, dataset, batch_size=256)
 
 features2, mapping2 = tools.get_dataset('full_dataset', 'SimCLR v2 ResNet50 2x')
 
